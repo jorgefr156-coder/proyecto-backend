39436bf92a2291cf581a504a9562d36461df8097
jorgefr156-coder <jorgefr156@gmail.com>
2026-05-04 03:10:20 -0400

    Tarea: Separación de modelo y repositorio MySQL.

---
+++ src/aplication/use-cases/note.service.js
@@ -0,0 +1,22 @@
+// importante: añadir .js al final para ESM
+import NoteEntity from "../../domain/entities/note.entity.js"; 
+
+export default class NoteService {
+    constructor(noteRepository) {
+        this.noteRepository = noteRepository;
+    }
+
+    async createNote(data) {
+        if (!data.title || !data.content) { 
+            throw new Error("Title and content are required"); 
+        }
+
+        const note = new NoteEntity(data);
+        return await this.noteRepository.save(note);
+    }
+
+    
+    async getNoteByUser(userId){
+        return await this.noteRepository.findByUserId(userId);
+    }
+} 
---
+++ src/infraestructure/database/mysql/note.model.js
@@ -0,0 +1,14 @@
+import { DataTypes } from "sequelize";
+import sequelize from "./connection.js";
+
+// Solo la definición de la estructura de la tabla
+const NoteModel = sequelize.define("Note", {
+    title: { type: DataTypes.STRING, allowNull: false },
+    content: { type: DataTypes.TEXT, allowNull: false },
+    imageUrl: { type: DataTypes.STRING },
+    isPrivate: { type: DataTypes.BOOLEAN, defaultValue: false },
+    password: { type: DataTypes.STRING },  
+    userId: { type: DataTypes.STRING, allowNull: false }
+}, { timestamps: true });
+
+export default NoteModel;
